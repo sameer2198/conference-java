b0d1db19dca3a9d5472acdea91609d69b04147d1
Ashutosh Sameer <ashutoshsameer@iitkgp.ac.in>
2020-03-29 12:49:28 +0530

    init-commit

---
+++ src/main/java/Application.java
@@ -0,0 +1,16 @@
+import com.pluralsight.service.SpeakerService;
+import com.pluralsight.service.SpeakerServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Application {
+
+    public static void main(String[] args) {
+        ApplicationContext appContext = new AnnotationConfigApplicationContext(AppConfig.class);
+//        SpeakerService service = new SpeakerServiceImpl();
+        SpeakerService service = appContext.getBean("speakerService", SpeakerService.class);
+
+        System.out.println(service.findAll().get(0).getLastName());
+        System.out.println(service.findAll().get(0).getSeedNum());
+    }
+}
